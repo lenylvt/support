47fd427cf8a1af873cf4a9f374d78f70274c225a
Tryon <lucas.lavajo@gmail.com>
2024-12-22 18:17:44 +0100

    fix(deps): Met à jour les dépendances Svelte et change l'adaptateur en Cloudflare

--- svelte.config.js
+++ svelte.config.js
@@ -1,20 +1,15 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-cloudflare';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://svelte.dev/docs/kit/integrations
-	// for more information about preprocessors
-	preprocess: [vitePreprocess()],
+	// Utilisation du preprocesseur Vite
+	preprocess: vitePreprocess(),
 
 	kit: {
-		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
+		// Configuration pour utiliser l'adaptateur Cloudflare
 		adapter: adapter()
-	},
-
-	extensions: ['.svelte']
+	}
 };
 
 export default config;
